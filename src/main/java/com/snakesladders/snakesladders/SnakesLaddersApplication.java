4ef73b5269c4e0f804cf3e495e03047d9bff1dfe
Artem Savchenko <artichsa@gmail.com>
2021-04-09 11:04:23 +0300

    Add messages for game

---
+++ src/main/java/com/snakesladders/snakesladders/SnakesLaddersApplication.java
@@ -0,0 +1,13 @@
+package com.snakesladders.snakesladders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakesLaddersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnakesLaddersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/snakesladders/snakesladders/SnakesladdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snakesladders.snakesladders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakesladdersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
