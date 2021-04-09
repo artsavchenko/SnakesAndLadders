153444803dd81d52d2d31e850c02a6c2df59c040
Artem Savchenko <artichsa@gmail.com>
2021-04-09 11:22:10 +0300

    Add tests for board service

---
+++ src/test/java/com/snakesladders/snakesladders/service/BoardServiceTest.java
@@ -0,0 +1,27 @@
+package com.snakesladders.snakesladders.service;
+
+import com.snakesladders.snakesladders.model.Board;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BoardServiceTest {
+
+    BoardService boardService;
+
+    @BeforeEach
+    public void init() {
+       this.boardService = new BoardService();
+    }
+
+    @Test
+    public void initializeBoard() {
+        Board board = boardService.initializeBoard(5);
+
+        Assertions.assertEquals(5, board.getLadders().entrySet().size());
+        Assertions.assertEquals(5, board.getSnakes().entrySet().size());
+        Assertions.assertTrue(board.getLadders().entrySet().stream().allMatch(e -> e.getKey() < e.getValue()));
+        Assertions.assertTrue(board.getSnakes().entrySet().stream().allMatch(e -> e.getKey() > e.getValue()));
+
+    }
+}
